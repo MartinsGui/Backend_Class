002c906509fbe2e8abb42454462e380c1e4a3a55
MartinsGui <guimartins.dev@gmail.com>
2026-02-19 22:50:38 -0300

    190226 | Add first routes

--- src/app.controller.js
+++ src/app.controller.js
@@ -1,15 +1,52 @@
-import { Controller, Dependencies, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from '@nestjs/common';
+//import { AppService } from './app.service';
 
-@Controller()
-@Dependencies(AppService)
+@Controller('library')
+//@Dependencies(AppService)
 export class AppController {
-  constructor(appService) {
-    this.appService = appService;
+  //constructor(appService) {
+  //this.appService = appService;
+  //}
+
+  #acervo;
+
+  constructor() {
+    this.#acervo= [];
+
+    this.#acervo[0] = {titulo:"Introducao ao JavaScrip",autor:"Huguinho Pato"};
+    this.#acervo[1] = {titulo:"NodeJS for Dummys",autor:"Zezinho Pato"};
+    this.#acervo[2] = {titulo:"Backends com NodeJS",autor:"Luizinho Pato"};
+    this.#acervo[3] = {titulo:"Arquitetura de Software",autor:"Zezinho Pato"};
+    this.#acervo[4] = {titulo:"Desenvolvendo para WEB",autor:"Huguinho Pato"};
+    this.#acervo[5] = {titulo:"Microservicos",autor:"Zezinho Pato"};
   }
 
   @Get()
   getHello() {
-    return this.appService.getHello();
+    return "Welcome to central library!";
   }
+
+  @Get('books')
+  getBooksList() {
+    return this.#acervo;
+  }
+
+  @Get('titles')
+  getTitlesList() {
+    const titles = [];
+    for (let i = 0; i < this.#acervo.length; i++) {
+      titles.push(this.#acervo[i].titulo);
+    }
+    return titles;
+  }
+
+  @Get('authors')
+  getAuthorsList() {
+    const author = [];
+    for (let i = 0; i < this.#acervo.length; i++) {
+      author.push(this.#acervo[i].autor)
+    }
+    return author;
+  }
+
 }
