f601d3bdb2ada8d352a6e86afe04c73c815b9909
MartinsGui <guimartins.dev@gmail.com>
2026-03-08 15:12:57 -0300

    [fix] 08032026 | fix main terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,40 +1,41 @@
 resource "aws_security_group" "backend_sg" {
+
   name = "backend-devops-sg"
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
+    from_port = 3000
+    to_port = 3000
+    protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 resource "aws_instance" "backend" {
 
-  ami           = "ami-0fc5d935ebf8bc3bc"
+  ami = "ami-0b6c6ebed2801a5cb"
   instance_type = "t2.micro"
 
   key_name = "devops-key"
 
-  security_groups = [
-    aws_security_group.backend_sg.name
+  vpc_security_group_ids = [
+    aws_security_group.backend_sg.id
   ]
 
-  user_data = file("setup.sh")
+  user_data = file("scripts/setup.sh")
 
   tags = {
     Name = "Backend_DevOps"
